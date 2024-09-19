d62e340196e24dcbd996fcfa58ccaf52b9a3310b
shalri <fsaonlinework777@gmail.com>
2024-09-19 10:18:57 +0800

    Add page data

---
+++ src/libs/data.js
@@ -0,0 +1,105 @@
+export const navLinks = [
+  {
+    label: "How we work",
+    href: "/",
+  },
+  {
+    label: "Blog",
+    href: "#",
+  },
+  {
+    label: "Account",
+    href: "#",
+  },
+  {
+    label: "View plans",
+    href: "#",
+  },
+];
+
+export const hero = {
+  heading: "Humanizing your insurance.",
+  copy: "Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that’s right for you. Ensure you and your loved ones are protected.",
+  calltoaction: "View plans",
+};
+
+export const features = [
+  {
+    heading: "Snappy Process",
+    description:
+      "Our application process can be completed in minutes, not hours. Don’t get stuck filling in tedious forms.",
+    icon: "/images/icon-snappy-process.svg",
+  },
+  {
+    heading: "Affordable Prices",
+    description:
+      "We don’t want you worrying about high monthly costs. Our prices may be low, but we still offer the best coverage possible.",
+    icon: "/images/icon-affordable-prices.svg",
+  },
+  {
+    heading: "People First",
+    description:
+      "Our plans aren’t full of conditions and clauses to prevent payouts. We make sure you’re covered when you need it.",
+    icon: "/images/icon-people-first.svg",
+  },
+];
+
+export const socialLinks = [
+  {
+    label: "Facebook",
+    href: "#",
+    icon: "/images/icon-facebook.svg",
+  },
+  {
+    label: "Twitter",
+    href: "#",
+    icon: "/images/icon-twitter.svg",
+  },
+  {
+    label: "Pinterest",
+    href: "#",
+    icon: "/images/icon-pinterest.svg",
+  },
+  {
+    label: "Instagram",
+    href: "#",
+    icon: "/images/icon-instagram.svg",
+  },
+];
+
+export const footerLinks = [
+  {
+    category: "Our company",
+    links: [
+      { label: "How we work", href: "/" },
+      { label: "Why Insure?", href: "#" },
+      { label: "View plans", href: "#" },
+      { label: "Reviews", href: "#" },
+    ],
+  },
+  {
+    category: "Help me",
+    links: [
+      { label: "FAQ", href: "#" },
+      { label: "Terms of use", href: "#" },
+      { label: "Privacy policy", href: "#" },
+      { label: "Cookies", href: "#" },
+    ],
+  },
+  {
+    category: "Contact",
+    links: [
+      { label: "Sales", href: "#" },
+      { label: "Support", href: "#" },
+      { label: "Live chat", href: "#" },
+    ],
+  },
+  {
+    category: "Others",
+    links: [
+      { label: "Careers", href: "#" },
+      { label: "Press", href: "#" },
+      { label: "Licenses", href: "#" },
+    ],
+  },
+];
