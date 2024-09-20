2b0f063c73d0b079cf31506cd3198e1178b57bbd
shalri <fsaonlinework777@gmail.com>
2024-09-20 10:46:53 +0800

    Features mobile layout

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -3,22 +3,32 @@ import Image from "next/image";
 
 export default function Features() {
   return (
-    <article className="">
-      <h2 className="">We&apos;re different</h2>
-      {features.map((feature) => (
-        <section className="" key={feature.heading}>
-          <div className="relative size-20">
-            <Image
-              src={feature.icon}
-              alt={feature.heading}
-              fill
-              className="object-contain"
-            />
-          </div>
-          <h3 className="">{feature.heading}</h3>
-          <p className="">{feature.description}</p>
-        </section>
-      ))}
+    <article className="pb-[9rem]">
+      <div className="mx-auto mb-10 mt-[8.75rem] h-[1px] w-[9rem] bg-ip-dark-violet" />
+      <h2 className="px-4 text-center text-[3rem] leading-[1.02] -tracking-[0.6]">
+        We&rsquo;re different
+      </h2>
+      <div className="mt-20 flex flex-col gap-y-[3.7rem] px-6">
+        {features.map((feature) => (
+          <section
+            className="flex flex-col items-center justify-center"
+            key={feature.heading}
+          >
+            <div className="relative size-[5.35rem]">
+              <Image
+                src={feature.icon}
+                alt={feature.heading}
+                fill
+                className="object-contain"
+              />
+            </div>
+            <h3 className="mt-8 text-center text-[1.68rem]">
+              {feature.heading}
+            </h3>
+            <p className="mt-4 px-2 text-center">{feature.description}</p>
+          </section>
+        ))}
+      </div>
     </article>
   );
 }
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,7 +4,6 @@ import HowWeWork from "./HowWeWork";
 
 export default function LandingPage() {
   return (
-    // {/* <main className="row-start-2 flex flex-col items-start gap-8 place-self-start justify-self-start sm:items-start"> */}
     <main>
       <Hero />
       <Features />
