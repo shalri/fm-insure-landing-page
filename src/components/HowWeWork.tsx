a961dec9b7ca1bf80c03479a264f13362bf31dbd
shalri <fsaonlinework777@gmail.com>
2024-09-20 18:24:27 +0800

    Adjust bg pattern layout

--- src/components/HowWeWork.tsx
+++ src/components/HowWeWork.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 export default function HowWeWork() {
   return (
-    <section className="relative mb-[12rem] px-6 text-ip-very-light-gray">
+    <section className="relative mb-[12rem] overflow-hidden px-6 text-ip-very-light-gray">
       <div className="absolute right-6 size-[316px] bg-[url(/images/bg-pattern-how-we-work-mobile.svg)] bg-right bg-no-repeat" />
       <div className="flex flex-col items-center bg-ip-dark-violet px-6 py-[4.5rem]">
         <h2 className="text-center text-[2.44rem] leading-[1.02]">
