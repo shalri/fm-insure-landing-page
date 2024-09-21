1c1fdd4384e3e41598a56438a8af44e979a27b32
shalri <fsaonlinework777@gmail.com>
2024-09-21 17:34:27 +0800

    Control flex in medium screens
    This is just a temporary solution for prototyping.
    Apply a more elegant solution in prod.

--- src/components/HowWeWork.tsx
+++ src/components/HowWeWork.tsx
@@ -5,13 +5,13 @@ export default function HowWeWork() {
   return (
     <section className="relative mb-[12rem] overflow-hidden px-6 text-ip-very-light-gray sm:mx-auto sm:max-w-[1156px]">
       <div className="absolute right-6 size-[316px] bg-[url(/images/bg-pattern-how-we-work-mobile.svg)] bg-right bg-no-repeat sm:h-[250px] sm:w-[435px] sm:bg-[url(/images/bg-pattern-how-we-work-desktop.svg)]" />
-      <div className="flex flex-col items-center bg-ip-dark-violet px-6 py-[4.4rem] sm:flex-row sm:justify-between sm:px-20">
-        <h2 className="text-center text-[2.44rem] leading-[1.02] sm:max-w-[480px] sm:text-left sm:text-[3.344rem] sm:leading-[1.03] sm:tracking-[0.01em]">
+      <div className="flex flex-col items-center bg-ip-dark-violet px-6 py-[4.4rem] sm:flex-row sm:justify-between sm:gap-x-4 sm:px-20">
+        <h2 className="line-clamp-2 text-center text-[2.44rem] leading-[1.02] sm:max-w-[480px] sm:text-left sm:text-[3.344rem] sm:leading-[1.03] sm:tracking-[0.01em]">
           {cta.copy}
         </h2>
         <Button
           label={cta.buttonCopy}
-          className="mt-10 px-[1.4rem] sm:mt-0 sm:py-[0.55rem]"
+          className="mt-10 text-nowrap px-[1.4rem] sm:mt-0 sm:shrink-0 sm:py-[0.55rem]"
         />
       </div>
     </section>
